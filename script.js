b2c9650673c898c47d2bb4a5f0ac0f4dce215e27
eaaprf <60609318+eaaprf@users.noreply.github.com>
2020-05-03 16:42:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,82 @@
-var success = new XMLHttpRequest();
-fetch('http://www.omdbapi.com/?s=alien&apikey=728657e2')
-    .then((success) => success.json())
-    .then((movies) => {
-        console.log(movies)
+//var success = new XMLHttpRequest();
+//fetch('http://www.omdbapi.com/?s=alien&apikey=728657e2')
+//    .then((success) => success.json())
+//    .then((movies) => {
+//        console.log(movies)
+//
+//        success.onload = function () {
+//            var success = request.success;
+//            var movies = JSON.parse(response);
+//
+//
+//            for (item in movies) {
+//
+//                //Display all the product names
+//                var title = movies[0].Title;
+//                var film = document.createElement('li');
+//                products.innerHTML = title;
+//                document.body.appendChild(film);
+//
+//
+//                // Display all the product images
+//                var imageUrl = movies[0].Poster;
+//                var images = document.createElement('img');
+//                images.setAttribute('src', imageUrl);
+//                document.body.appendChild(images);
+//
+//            }
+//        }
+//    })
+//
+//    .catch((err) => {
+//        console.log(err)
+//    });
 
-        success.onload = function () {
-            var success = request.success;
-            var movies = JSON.parse(response);
 
 
-            for (item in movies) {
+// target the input & form
+const div = document.querySelector('div');
 
-                //Display all the product names
-                var title = movies[0].Title;
-                var film = document.createElement('li');
-                products.innerHTML = title;
-                document.body.appendChild(film);
+const createMovies = (moviesArray) => {
+  console.log(moviesArray);
+  const ul = document.querySelector('ul');
+
+  // clear ul everytime
+  ul.innerText = ''
+
+  moviesArray.forEach(movie => {
+    // create li
+    const li = document.createElement('li')
+    li.innerHTML = `<h3>${movie.Title} - ${movie.Year}</h3>`;
+
+    // create img only if movie has Poster
+    if(movie.Poster !== 'N/A') {
+      const img = document.createElement('img');
+      img.setAttribute('src', movie.Poster);      
+      
+      // insert img into li
+      li.insertAdjacentElement('beforeend', img);
+    }
 
 
-                // Display all the product images
-                var imageUrl = movies[0].Poster;
-                var images = document.createElement('img');
-                images.setAttribute('src', imageUrl);
-                document.body.appendChild(images);
+    // insert li to ul
+    ul.insertAdjacentElement('beforeend', li);
+  })
+}
 
-            }
-        }
+const callApi = (url) => {
+  fetch(url)
+    .then(response => response.json())
+    .then(movies => {
+      createMovies(movies.Search);
     })
+}
 
-    .catch((err) => {
-        console.log(err)
-    });
+
+  const url = `https://www.omdbapi.com/?s=alien&apikey=728657e2`
+
+  // call the api
+  callApi(url);
+
+
+form.addEventListener('submit', onSubmit);
