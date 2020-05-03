e71ba234c84f06d114be5c4c81897d655afb4b13
eaaprf <60609318+eaaprf@users.noreply.github.com>
2020-05-03 16:22:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,30 +2,41 @@ var success = new XMLHttpRequest();
 fetch('http://www.omdbapi.com/?s=alien&apikey=728657e2')
     .then((success) => success.json())
     .then((movies) => {
-        console.log(movies)
-
+        console.log(createMovies)
+});
         success.onload = function () {
             var success = request.success;
-            var movies = JSON.parse(response);
-
-            for (item in movies) {
-
-                //Display all the product names
-                var title = movies[0].Title;
-                var film = document.createElement('li');
-                products.innerHTML = title;
-                document.body.appendChild(film);
+            var createMovies = JSON.parse(response);
 
 
-                // Display all the product images
-                var imageUrl = movies[0].Poster;
-                var images = document.createElement('img');
-                images.setAttribute('src', imageUrl);
-                document.body.appendChild(images);
+            const createMovies = (moviesArray) => {
+  console.log(moviesArray);
+  const ul = document.querySelector('ul');
 
-            }
-        }
-    })
+  // clear ul everytime
+  ul.innerText = ''
+
+  moviesArray.forEach(movie => {
+    // create li
+    const li = document.createElement('li')
+    li.innerHTML = `<h3>${movie.Title} - ${movie.Year}</h3>`;
+
+    // create img only if movie has Poster
+    if(movie.Poster !== 'N/A') {
+      const img = document.createElement('img');
+      img.setAttribute('src', movie.Poster);      
+      
+      // insert img into li
+      li.insertAdjacentElement('beforeend', img);
+    }
+
+
+    // insert li to ul
+    ul.insertAdjacentElement('beforeend', li);
+  })
+}
+        
+    }
 
     .catch((err) => {
         console.log(err)
